ab953f40f28074d78b995a368343826349736e31
pedrohgarbim <phg.garbim@gmail.com>
2024-09-08 19:46:56 -0300

    Adicionando projetos de testes

---
+++ Calculator/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Flight/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Flight
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
