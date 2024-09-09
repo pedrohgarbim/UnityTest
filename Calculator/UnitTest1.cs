d4eddd91d479436cc86478e8e20154513bfe6640
pedrohgarbim <phg.garbim@gmail.com>
2024-09-08 21:48:10 -0300

    Calculator Project

--- Calculator/UnitTest1.cs
+++ Calculator/UnitTest1.cs
@@ -1,11 +1,17 @@
-﻿namespace Calculator
+﻿using System;
+using Xunit;
+using Domain;
+
+namespace CalculatorTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestPassed() 
         {
-
-        }
+            var calculator = new Calculator();
+            if (calculator.Sum(2, 2) != 4)
+                throw new Exception();
+        }     
     }
 }
---
+++ Domain/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class Calculator
+    {
+         public int Sum(int left, int right)
+        {
+            return left + right;
+        }
+
+    }
+}
