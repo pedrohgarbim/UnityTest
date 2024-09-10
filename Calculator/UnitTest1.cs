2cffac3a4bd5dc91e810b637ab9bee5b92af1c8e
pedrohgarbim <phg.garbim@gmail.com>
2024-09-10 13:04:35 -0300

    Assertion Message

--- Calculator/UnitTest1.cs
+++ Calculator/UnitTest1.cs
@@ -14,7 +14,8 @@ namespace CalculatorTest
             var result = calculator.Sum(2, 2);
             if (result != 4)
             {
-                throw new Exception();
+                throw new Exception($"The Sum(2,2) was expected to be 4" +
+                    $"but it's {result}");
             }
         }
 
