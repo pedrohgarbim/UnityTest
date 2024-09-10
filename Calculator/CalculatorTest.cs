e190319687f7d79652eac2cb57309ba3fad38793
pedrohgarbim <phg.garbim@gmail.com>
2024-09-10 13:10:14 -0300

    Fluent Assertions

---
+++ Calculator/CalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using FluentAssertions;
+using System;
+using Xunit;
+
+
+namespace CalculatorTest
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void Sum_of_2_and_2_should_be_4()
+         => new Calculator()
+            .Sum(2, 2)
+            .Should().Be(4);
+
+        
+    }
+}
--- Domain/Calculator.cs
+++ Domain/Calculator.cs
@@ -9,8 +9,7 @@ namespace Domain
     public class Calculator
     {
          public int Sum(int left, int right)
-        {
-            return left + right;
-        }
+            => left + right;
+        
     }
 }
